f73d79aa282f1cde9de34e7879120110474772ab
GSE <carlos.gutierrez@bemyself.com>
2025-02-15 10:44:44 -0500

    feature login

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:storeapp/app/main_app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MainApp();
-  }
+  runApp(const MainApp());
 }
