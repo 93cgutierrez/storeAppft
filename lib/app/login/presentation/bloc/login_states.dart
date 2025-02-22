f6530626ea58847452ebf7af129f1acf0990dbef
GSE <carlos.gutierrez@bemyself.com>
2025-02-22 10:54:43 -0500

    feature login CG 20250222 -- add bloc dependency create files: login_bloc.dart login_events.dart login_form_model.dart login_states.dart -- Testing Android OK

---
+++ lib/app/login/presentation/bloc/login_states.dart
@@ -0,0 +1,26 @@
+import 'package:storeapp/app/login/presentation/model/login_form_model.dart';
+
+sealed class LoginState {
+  final LoginFormModel model;
+
+  LoginState({
+    required this.model,
+  });
+}
+
+final class InitialState extends LoginState {
+  InitialState({
+    required LoginFormModel model,
+  }) : super(
+          model: LoginFormModel(
+            email: '',
+            password: '',
+          ),
+        );
+}
+
+final class DataUpdatedState extends LoginState {
+  DataUpdatedState({
+    required super.model,
+  });
+}
---
+++ lib/app/login/presentation/model/login_form_model.dart
@@ -0,0 +1,9 @@
+final class LoginFormModel {
+  final String email;
+  final String password;
+
+  LoginFormModel({
+    required this.email,
+    required this.password,
+  });
+}
